dd72a83f3f66855bf7dbaa1d3f7ae3050f868ac2
S-Cgrass <55120402+S-Cgrass@users.noreply.github.com>
2023-10-19 17:02:04 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+from socket import timeout
+import sys
+
+import json
+import requests
+import datetime
+
+now=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+now_str=str(now)
+# content1="test message"+ now
+content2="testmessage" + now_str
+print(now)
+print(now_str)
+print(content2)
